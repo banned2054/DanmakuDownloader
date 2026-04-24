ffac17753a67cb6281465817b21983e4dbdb7615
banned2054 <banned2054@163.com>
2026-04-24 23:02:54 +0800

    test: 添加单元测试项目

---
+++ DanmakuDownloader.Test/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using DanmakuDownloader.Web.Utils;
+
+namespace DanmakuDownloader.Test;
+
+public class Tests
+{
+    private static readonly string BaseUrl = Environment.GetEnvironmentVariable("JellyfinUrl") ??
+                                             throw new InvalidOperationException("Jellyfin Url is Null");
+
+    private static readonly string UserName = Environment.GetEnvironmentVariable("JellyfinUserName") ??
+                                              throw new InvalidOperationException("Jellyfin User Name is Null");
+
+    private static readonly string Password = Environment.GetEnvironmentVariable("JellyfinPassword") ??
+                                              throw new InvalidOperationException("Jellyfin Password is Null");
+
+    [SetUp]
+    public void Setup()
+    {
+        DotNetEnv.Env.Load();
+    }
+
+    [Test]
+    public async Task Test1()
+    {
+        var jellyfin = new JellyfinUtils(BaseUrl, UserName, Password);
+        await jellyfin.Login();
+        await jellyfin.GetMediaFolderList();
+    }
+}
