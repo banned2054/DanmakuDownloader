681dede45ca131f3d09648bc9a42730a89c83753
banned2054 <banned2054@163.com>
2026-04-25 18:44:42 +0800

    feat: 新增 DanmakuFilterController 弹幕过滤控制器

---
+++ DanmakuDownloader.Web/Controllers/DanmakuFilterController.cs
@@ -0,0 +1,37 @@
+﻿using DanmakuDownloader.Web.Models.Danmaku;
+using DanmakuDownloader.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DanmakuDownloader.Web.Controllers;
+
+[ApiController]
+[Route("api/v1/danmaku-filter")]
+public class DanmakuFilterController(DanmakuFilterService filterService) : ControllerBase
+{
+    [HttpGet]
+    public List<DanmakuFilter> GetFilters()
+    {
+        return filterService.GetRules();
+    }
+
+    [HttpPut]
+    public IActionResult SetFilters([FromBody] List<DanmakuFilter> newFilters)
+    {
+        filterService.UpdateRules(newFilters);
+        return NoContent();
+    }
+
+    [HttpPost]
+    public IActionResult InsertFilters([FromBody] List<DanmakuFilter> filters)
+    {
+        filterService.InsertRules(filters);
+        return NoContent();
+    }
+
+    [HttpPost("delete")]
+    public IActionResult DeleteFilters([FromBody] List<DanmakuFilter> filters)
+    {
+        filterService.DeleteRules(filters);
+        return NoContent();
+    }
+}
