92361baa4b826e918a2f2f6a7935a279311519b5
banned2054 <banned2054@163.com>
2026-04-25 18:44:34 +0800

    refactor: 简化 DanmakuController，移除过滤相关 API

--- DanmakuDownloader.Web/Controllers/DanmakuController.cs
+++ DanmakuDownloader.Web/Controllers/DanmakuController.cs
@@ -1,37 +1,5 @@
-﻿using DanmakuDownloader.Web.Models.Danmaku;
-using DanmakuDownloader.Web.Services;
-using Microsoft.AspNetCore.Mvc;
+﻿namespace DanmakuDownloader.Web.Controllers;
 
-namespace DanmakuDownloader.Web.Controllers;
-
-[ApiController]
-[Route("api/v1/danmaku")]
-public class DanmakuController(DanmakuFilterService filterService) : ControllerBase
+public class DanmakuController
 {
-    [HttpGet("filter")]
-    public List<DanmakuFilter> GetFilters()
-    {
-        return filterService.GetRules();
-    }
-
-    [HttpPut("filter")]
-    public IActionResult SetFilters([FromBody] List<DanmakuFilter> newFilters)
-    {
-        filterService.UpdateRules(newFilters);
-        return NoContent();
-    }
-
-    [HttpPost("filter/insert")]
-    public IActionResult InsertFilter([FromBody] DanmakuFilter filter)
-    {
-        filterService.InsertRule(filter);
-        return NoContent();
-    }
-
-    [HttpPost("filter/insert-batch")]
-    public IActionResult InsertFilters([FromBody] List<DanmakuFilter> filters)
-    {
-        filterService.InsertRules(filters);
-        return NoContent();
-    }
 }
